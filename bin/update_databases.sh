48430173b6db430a8a582871d143d3f271e98c87
Johannes Deger <johannes.deger@uni-ulm.de>
2019-08-14 15:08:44 +0200

    Fixed Typo

--- bin/update_databases.sh
+++ bin/update_databases.sh
@@ -9,13 +9,13 @@
 cd /mount/storage/db/irr
 for f in *.db*; do mv "$f" old/""${f%}"_$(date '+%Y_%m_%d-%H')"; done
 
-wget http://ftp.afrinic.net/pub/dbase/afrinic.db.gz
-wget ftp://ftp.arin.net/pub/rr/arin.db
-wget ftp://ftp.radb.net/radb/dbase/radb.db.gz
-wget ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.route.gz
-wget ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.route6.gz
-wget ftp://ftp.apnic.net/pub/apnic/whois/apnic.db.route.gz
-wget ftp://ftp.apnic.net/pub/apnic/whois/apnic.db.route6.gz
+wget -4  http://ftp.afrinic.net/pub/dbase/afrinic.db.gz
+wget -4  ftp://ftp.arin.net/pub/rr/arin.db
+wget -4  ftp://ftp.radb.net/radb/dbase/radb.db.gz
+wget -4  ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.route.gz
+wget -4  ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.route6.gz
+wget -4  ftp://ftp.apnic.net/pub/apnic/whois/apnic.db.route.gz
+wget -4  ftp://ftp.apnic.net/pub/apnic/whois/apnic.db.route6.gz
 
 gunzip ./*
 
@@ -23,5 +23,5 @@ cd ../rpki
 for f in *; do mv "$f" old/""${f%}"_$(date '+%Y_%m_%d-%H')"; done
 routinator vrps > current
 
-/home/debian/ba/bin/luke_filewalker.pl
+/home/debian/ba/bin/luke_dbwalker.pl
 systemctl reload ris-live
