5d92c58c6e70c55797d6bcb1d784bf544afe7a22
Johannes Deger <johannes.deger@uni-ulm.de>
2019-10-11 17:14:08 +0200

    Naming inconsistency fixed

--- bin/ris-live-historic/parallel_irr_parser.sh
+++ bin/ris-live-historic/parallel_irr_parser.sh
@@ -1,1 +1,1 @@
-find /mount/storage/db/irr/old | egrep -o '2019_[0-9]+_[0-9]+-00' | sort -n | uniq | xargs -L 1 -P 5 -I % bash -c './parse_irrs.pl %' | tee /mount/storage/stash/historic/stats/hist_irr_repo_sizes.txt 
+find /mount/storage/db/irr/old | egrep -o '2019_[0-9]+_[0-9]+-00' | sort -n | uniq | xargs -L 1 -P 5 -I % bash -c './irr_parser.pl %' | tee /mount/storage/stash/historic/stats/hist_irr_repo_sizes.txt 
