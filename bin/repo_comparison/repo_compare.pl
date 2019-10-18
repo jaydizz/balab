8dd2c06409570672bbf1b2f2ff54e8c9294a39ac
Johannes Deger <johannes.deger@uni-ulm.de>
2019-10-18 23:39:45 +0200

    repo_comparer now loggs detailedly

--- bin/repo_comparison/repo_compare.pl
+++ bin/repo_comparison/repo_compare.pl
@@ -27,7 +27,8 @@ use Local::addrinfo qw( by_cidr mk_iprange_lite mk_iprange is_subset);
 use 5.26.1;
 
 my $file = shift;
-
+my $as   = shift or 0;
+chomp($as);
 $Net::Validator::LOG_INVALIDS = 1;
 
 
@@ -38,7 +39,8 @@ if ( $file =~ /(20\d{2})-(\d{2})-(\d{2})-irrv4\.storable/ ) {
 } else {
   exit(0);
 }
-open ($Net::Validator::INV_LOG, '>', "/tmp/$year-$month-$day.invalids");
+open ($Net::Validator::INV_LOG, '>', "/mount/storage/stash/repo_compare/$year-$month-$day.invalids");
+open (my $not_founds_log , '>', "/mount/storage/stash/repo_compare/$year-$month-$day.not_found");
 
 my $pt_v4 = retrieve $file or die("Could not load v4 trie");
 $file =~ s/irrv4/irrv6/;
@@ -51,7 +53,7 @@ my $date = "$year/$month/$day";
 @roas = grep { /roas\.csv/ } @roas;
 @roas = grep { /$date/ } @roas;
 
-return if (!scalar @roas);
+exit(0) if (!scalar @roas);
 
 my $result_hash = {
       invalid    => 0,
@@ -60,6 +62,9 @@ my $result_hash = {
       valid_impl => 0,
       valid_ls   => 0,
 };
+
+my $as_stats;
+
 my $total = 0;
 
 
@@ -73,24 +78,39 @@ foreach my $file (@roas) {
     $total++;
     chomp($prefix);
     chomp($origin_as);
-    #$DB::single = 1; 
     my $return = validate_irr($prefix, $origin_as, $pt_v4, $pt_v6);
-    $DB::single = 1;
+    
+    if ( $return->{not_found} && $max_length ge 1) { #Let's not give up yet!
+      my $cidr = (split /\//, $prefix)[0];
+      my $return2 = validate_irr("$cidr/$max_length", $origin_as, $pt_v4, $pt_v6);
+      if ( !$return2->{invalid} && !$return2->{not_found} ) {
+        $return = $return2;
+      } else {
+        say "$prefix  : $origin_as not found." if $origin_as eq $as;
+        $as_stats->{$origin_as} = 0 unless $as_stats->{$origin_as};
+        $as_stats->{$origin_as} += 1;
+      }
+    }
     add_hashes($return);
   }
   close $FH;
 }
 
 
+foreach my $as (keys %$as_stats ) {
+    say $not_founds_log  "$as,$as_stats->{$as}";
+}
+close ($not_founds_log);
 #say "Total: $total";
 #foreach my $key (sort keys %$result_hash) {
 #  print $key . ",";
 #}
-print "\n";
+my $result = "$year-$month-$day,";
 foreach my $key (sort keys %$result_hash) {
-  print $result_hash->{$key}/$total*100 . ",";
+  next if $key eq "pt";
+  $result = $result .  $result_hash->{$key}/$total*100 . ",";
 }
-
+say $result;
 
 
 
